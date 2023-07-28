dd17d5cfc2e2179e3bf5d4a56b0a9704dbcde3c8
unknown <o.adrian.rodrigues@gmail.com>
2023-07-28 16:13:24 -0300

    Create DepartmentDaoJDBC, findById implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -106,9 +106,9 @@ public class SellerDaoJDBC implements SellerDao {
 		PreparedStatement st = null;
 		ResultSet rs = null;
 		try {
-			st = conn.prepareStatement(
-					"SELECT seller.*,department.Name as DepName " + "FROM seller INNER JOIN department "
-							+ "ON seller.DepartmentId = department.Id " + "WHERE seller.Id = ?");
+			st = conn.prepareStatement("SELECT seller.*,department.Name as DepName " 
+										+ "FROM seller INNER JOIN department "
+										+ "ON seller.DepartmentId = department.Id " + "WHERE seller.Id = ?");
 			st.setInt(1, id);
 			rs = st.executeQuery();
 			if (rs.next()) {
