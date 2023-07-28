448742584ccb68dc614ad8002d01ada440cc34cf
unknown <o.adrian.rodrigues@gmail.com>
2023-07-28 10:50:50 -0300

    Updated implement

--- src/application/Program.java
+++ src/application/Program.java
@@ -36,6 +36,11 @@ public class Program {
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id= " + newSeller.getId());
 		
+		System.out.println("\n=== TEST 4: seller insert ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Wayne");
+		sellerDao.update(seller);
+		System.out.println("Update Completed!");
 		
 	}
 
